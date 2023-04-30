56ef964f8398e1e6a98e45903725bb328b1c2b76
TheNotoriousB <104720658+TheNotoriousB@users.noreply.github.com>
2023-04-30 18:52:48 +0200

    Update query_9.sql

--- query_9.sql
+++ query_9.sql
@@ -2,7 +2,7 @@ SELECT COUNT(DISTINCT m.INAMI)
 FROM medecin m
 JOIN prescription pr ON pr.INAMI_med = m.INAMI
 JOIN patient p ON p.NISS = pr.NISS
-WHERE p.NISS = "NISS du patient concerné";
+WHERE p.NISS = {placeholder};
 
 /*
 Pour chaque patient,le nombre de médecin lui ayant prescrit
