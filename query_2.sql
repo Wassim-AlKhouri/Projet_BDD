58c82a2e9e08da19f4b15efeaafef92748dffe45
TheNotoriousB <104720658+TheNotoriousB@users.noreply.github.com>
2023-04-30 18:50:40 +0200

    Update query_2.sql

--- query_2.sql
+++ query_2.sql
@@ -1,7 +1,7 @@
 SELECT DISTINCT p.nom
 FROM pathologie p
 JOIN systeme_anatomique s ON p.NomSA = s.Nom
-WHERE s.nom = 'nom de la spécialité recherchée'
+WHERE s.nom = {placeholder}
 GROUP BY p.nom
 HAVING COUNT(DISTINCT Systeme_anatomique) = 1;
 
