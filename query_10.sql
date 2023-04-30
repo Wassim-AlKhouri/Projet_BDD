b0266bb00b7046aa71e632397e02f32c04c7e849
TheNotoriousB <104720658+TheNotoriousB@users.noreply.github.com>
2023-04-30 18:50:14 +0200

    Update query_10.sql

--- query_10.sql
+++ query_10.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT med.NomC
 FROM Medicament med
 JOIN Prescription p ON med.NomC = p.NomC
 JOIN traitement t ON t.DCI=m.DCI
-WHERE t.Date_de_debut <= "date recherchée"
+WHERE t.Date_de_debut <= {placeholder}
 
 /*
 La liste de médicament n’étant plus prescrit depuis 
