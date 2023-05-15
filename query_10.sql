40cc3bab904faeab97a81fbd2d46af5ba648d621
Wassim-AlKhouri <Wassim.Al.Khouri@ulb.be>
2023-05-15 09:22:38 +0200

    correction quer 10

--- query_10.sql
+++ query_10.sql
@@ -1,3 +1,8 @@
 SELECT DISTINCT d.medicamentNomCommercial
 FROM DossierPatient d
-WHERE d.datePrescription <= '{placeholder1}'
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM DossierPatient d2
+  WHERE d2.medicamentNomCommercial = d.medicamentNomCommercial
+    AND d2.datePrescription > '{placeholder1}'
+)
