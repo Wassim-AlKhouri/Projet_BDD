f46b14199f77d82d262cff57e926c2afab8ad7a0
Wassim-AlKhouri <Wassim.Al.Khouri@ulb.be>
2023-05-21 15:16:44 +0200

    update quer 3

--- query_3.sql
+++ query_3.sql
@@ -4,5 +4,5 @@ JOIN SpecialiteSystemeAnatomique a ON s.specialiteNom = a.SpecialiteNom
 JOIN Medicament m ON m.systemeAnatomiqueNom=a.systemeAnatomiqueNom
 JOIN DossierPatient d ON d.medicamentNomCommercial=m.medicamentNomCommercial
 GROUP BY s.specialiteNom
-ORDER BY COUNT(*) DESC
+ORDER BY COUNT(DISTINCT m.medicamentNomCommercial) DESC
 LIMIT 1
