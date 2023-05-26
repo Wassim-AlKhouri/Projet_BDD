f3a88bafb8f41a21d5e683c6fc41f892466f55d5
Wassim-AlKhouri <Wassim.Al.Khouri@ulb.be>
2023-05-26 17:03:34 +0200

    corr query 7 alt

--- queries/query_7_alt.sql
+++ queries/query_7_alt.sql
@@ -1,7 +1,7 @@
 SELECT med.medicamentNomCommercial
 FROM Medicament med
 JOIN DossierPatient d ON d.medicamentNomCommercial=med.medicamentNomCommercial
-JOIN Patient p ON P.NISS=d.NISS
+JOIN Patient p ON p.NISS=d.NISS
 WHERE p.DateNaissance > '{placeholder1}' AND p.DateNaissance < '{placeholder2}' + 10 
 GROUP BY med.medicamentNomCommercial
 ORDER BY COUNT(*) DESC
