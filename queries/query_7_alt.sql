8a587d9dc072a5dc6e008893863d922aeddb9df4
Wassim-AlKhouri <Wassim.Al.Khouri@ulb.be>
2023-05-26 16:37:42 +0200

    remise de l'ancien query 7

---
+++ queries/query_7_alt.sql
@@ -0,0 +1,8 @@
+SELECT med.medicamentNomCommercial
+FROM Medicament med
+JOIN DossierPatient d ON d.medicamentNomCommercial=med.medicamentNomCommercial
+JOIN Patient p ON P.NISS=d.NISS
+WHERE p.DateNaissance > '{placeholder1}' AND p.DateNaissance < '{placeholder2}' + 10 
+GROUP BY med.medicamentNomCommercial
+ORDER BY COUNT(*) DESC
+LIMIT 1;
